5d813443f7f002116e39efe8f5f311a60ca80527
Rafael Camargo <hello@rafaelcamargo.com>
2023-06-02 15:04:16 -0300

    feat: improve border preview styles

--- src/home/components/border-preview/border-preview.js
+++ src/home/components/border-preview/border-preview.js
@@ -1,9 +1,17 @@
+import './border-preview.styl';
 import { useSharedBorders } from '@src/home/hooks/shared-borders/shared-borders';
 import { buildStyle } from '@src/home/services/border-style/border-style';
 
 export const BorderPreview = () => {
   const { borders } = useSharedBorders();
   return (
-    <div title="preview" style={buildStyle(borders)}></div>
+    <div className="b-border-preview-container">
+      <div
+        className="b-border-preview"
+        title="preview"
+        style={buildStyle(borders)}
+      >
+      </div>
+    </div>
   );
 };
